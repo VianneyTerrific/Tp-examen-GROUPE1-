e644d0c71e0cfd3efa2ecc9904ed0a14dbd32867
AMANI_Jonam <jonam23b@gmail.com>
2025-04-22 16:51:53 +0200

    Création de la classe DGVPrinter et ajout des scripts db

---
+++ Database_Script/Database.sql
@@ -0,0 +1,55 @@
+create database gestion_personne
+go
+
+use gestion_personne
+go
+
+create table personne
+(
+	id varchar(50),
+	nom varchar(50) not null,
+	postnom varchar(50)not null,
+	prenom varchar(50),
+	sexe varchar(1) default 'M' not null,
+	constraint pk_personne primary key(id),
+	constraint uk_personne unique(nom,postnom,prenom)
+)
+go
+
+create table adresse
+(
+	id varchar(50),
+	quartier varchar(50) not null,
+	commune varchar(50) not null,
+	ville varchar(50) not null,
+	pays varchar(50) not null,
+	constraint pk_adresse primary key(id)
+)
+go
+
+create table domicile
+(
+	id varchar(50),
+	id_personne varchar(50) not null,
+	id_adresse varchar(50) not null,
+	avenue varchar(50) not null,
+	numero_avenue int not null,
+	constraint pk_domicile primary key(id),
+	constraint fk_personne_domicile foreign key(id_personne)
+	references personne(id),
+	constraint fk_addresse_domicile foreign key(id_adresse)
+	references adresse(id)
+)
+go
+
+create table telephone 
+(
+	id varchar(50),
+	id_personne varchar(50) not null,
+	initial varchar(4) not null,
+	numero varchar(9) not null,
+	constraint pk_telephone primary key(id),
+	constraint fk_personne_telephone 
+	foreign key(id_personne) references personne(id)
+)
+go
---
+++ Database_Script/ProcédureStockée.sql
@@ -0,0 +1,175 @@
+--Ajout personne
+create procedure sp_AjouterPersonne
+	@id varchar(50),@nom varchar(50),@postnom varchar(50),
+	@prenom varchar(50),@sexe varchar(1)
+as
+begin
+	if not exists(select * from personne where id=@id)
+		insert into personne(id,nom,postnom,prenom,sexe) values 
+		(@id,@nom,@postnom,@prenom,@sexe)
+end
+go
+
+--Modification personne
+create procedure sp_ModifierPersonne
+	@id varchar(50),@nom varchar(50),@postnom varchar(50),
+	@prenom varchar(50),@sexe varchar(1)
+as
+begin
+	update personne set nom=@nom,postnom=@postnom,prenom=@prenom,
+	sexe=@sexe where id=@id
+end
+go
+
+--Suppression personne
+create procedure sp_SupprimerPersonne
+	@id varchar(50)
+as
+begin
+	if exists(select * from personne where id=@id)
+		delete from personne where id=@id
+end
+go
+
+--Affichage personne
+create procedure sp_AfficherPersonne
+as
+begin 
+	select * from personne order by nom asc
+end
+go
+
+
+
+--Ajout adresse
+create procedure sp_AjouterAdresse
+	@id varchar(50),@quartier varchar(50),@commune varchar(50),
+	@ville varchar(50),@pays varchar(50)
+as
+begin
+	if not exists(select * from adresse where id=@id)
+		insert into adresse(id,quartier,commune,ville,pays) values 
+		(@id,@quartier,@commune,@ville,@pays)
+end
+go
+
+--Modification adresse
+create procedure sp_ModifierAdresse
+	@id varchar(50),@quartier varchar(50),@commune varchar(50),
+	@ville varchar(50),@pays varchar(50)
+as
+begin
+	update adresse set quartier=@quartier,commune=@commune,
+	ville=@ville,pays=@pays where id=@id
+end
+go
+
+--Suppression adresse
+create procedure sp_SupprimerAdresse
+	@id varchar(50)
+as
+begin
+	if exists(select * from adresse where id=@id)
+		delete from adresse where id=@id
+end
+go
+
+--Affichage adresse
+create procedure sp_AfficherAdresse
+as
+begin
+	select * from adresse order by quartier asc
+end
+go
+
+
+
+--Ajout domicile
+create procedure sp_AjouterDomicile
+	@id varchar(50),@id_personne varchar(50),@id_adresse varchar(50),@avenue varchar(50),
+	@numero_avenue int
+as
+begin
+	if not exists(select * from domicile where id=@id)
+		insert into domicile(id,id_personne,id_adresse,avenue,numero_avenue) values 
+		(@id,@id_personne,@id_adresse,@avenue,@numero_avenue)
+end
+go
+
+--Modification domicile
+create procedure sp_ModifierDomicile
+	@id varchar(50),@id_personne varchar(50),@id_adresse varchar(50),@avenue varchar(50),
+	@numero_avenue int
+as
+begin
+	update domicile set id_personne=@id_personne,id_adresse=@id_adresse,avenue=@avenue,
+	numero_avenue=@numero_avenue where id=@id
+end
+go
+
+--Suppression domicile
+create procedure sp_SupprimerDomicile
+	@id varchar(50)
+as
+begin
+	if exists(select * from domicile where id=@id)
+		delete from domicile where id=@id
+end
+go
+
+--Affichage domicile
+create procedure sp_AfficherDomicile
+as
+begin 
+	select d.id,id_personne,nom+' '+postnom+' '+prenom as noms,id_adresse,pays+','+ville+','+commune+','+quartier as adresse,
+		avenue,numero_avenue from domicile d 
+	inner join personne p on p.id = d.id_personne
+	inner join adresse a on a.id = d.id_adresse
+	order by nom asc
+end
+go
+
+
+
+--Ajout telephone
+create procedure sp_AjouterTelephone
+	@id varchar(50),@id_personne varchar(50),@initial varchar(4),
+	@numero varchar(9)
+as
+begin
+	if not exists(select * from telephone where id=@id)
+		insert into telephone(id,id_personne,initial,numero) values 
+		(@id,@id_personne,@initial,@numero)
+end
+go
+
+--Modification telephone
+create procedure sp_ModifierTelephone
+	@id varchar(50),@id_personne varchar(50),@initial varchar(4),
+	@numero varchar(9)
+as
+begin
+	update telephone set id_personne=@id_personne,initial=@initial,
+	numero=@numero where id=@id
+end
+go
+
+--Suppression telephone
+create procedure sp_SupprimerTelephone
+	@id varchar(50)
+as
+begin
+	if exists(select * from telephone where id=@id)
+		delete from telephone where id=@id
+end
+go
+
+--Affichage telephone
+create procedure sp_AfficherTelephone
+as
+begin
+	select t.id,id_personne,nom+' '+postnom+' '+prenom as noms,initial,numero from telephone t 
+	inner join personne p on p.id = t.id_personne
+	order by nom asc
+end
+go
